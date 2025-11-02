a99234f8ced5509abc21fa137cb2e2f98b36c424
hzx-bit <3310087607@qq.com>
2025-11-02 20:31:07 +0800

    fix: 修复文件引用问题

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,4 +1,4 @@
-export { default as debounce } from './debounce';
-export { default as throttle } from './throttle';
-export { default as curry } from './curry';
-export { default as memoize } from './memoize';
+export { default as debounce } from './debounce.js';
+export { default as throttle } from './throttle.js';
+export { default as curry } from './curry.js';
+export { default as memoize } from './memoize.js';
